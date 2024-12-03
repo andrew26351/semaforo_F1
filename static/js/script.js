680d12e94e1bfc210f18d3b4a373f4a50ab6f51f
andrew2635 <andrewleone2007@gmail.com>
2024-12-03 07:37:20 +0100

    1

---
+++ static/js/script.js
@@ -0,0 +1,17 @@
+function timer(){
+
+}
+
+function handleClick(){
+    console.log("1")
+}
+
+
+
+let btn = document.querySelector("#start-button")
+let output = document.querySelector("#output")
+
+
+
+
+btn.addEventListener("click", handleClick);
